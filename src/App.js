949b9257b04f19c5825ac3291dfc93c6f9fcbd3b
Hironmoy02 <hironmoychowdhury69@gmail.com>
2023-09-28 00:39:25 +0530

    fix issues

--- src/App.js
+++ src/App.js
@@ -1,21 +1,102 @@
-
-import './App.css';
-import About from './Components/About';
+import React, { useState } from "react";
+import "./App.css";
+import About from "./Components/About";
 import Navbar from "./Components/Navbar";
-import TextForm from './Components/TextForm';
+import TextForm from "./Components/TextForm";
+import Alert from "./Components/Alert";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
 
 function App() {
-  return (
-    <>
-   
-<Navbar title="Hironmoy"/>
-<div className='container'>
-<TextForm heading="what's on your mind"/>
-<About/>
-</div>
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+  const toggleMode = () => {
+    if (mode === "light") {
+      // setMode("dark");
+      // document.body.style.backgroundColor = "grey";
+      showAlert("dark mode enabled", "success");
+    } else if (mode === "dark") {
+      // setMode("light");
+      // document.body.style.backgroundColor = "white";
+      showAlert("light mode enabled", "success");
+    }
+    // else if(mode==="light"||"dark"){
+    //   setMode("green");
+    //   document.body.style.backgroundColor="#234F1E"
+    //   showAlert("light mode enabled","success");}
+  };
 
-    </>
+  // const router = createBrowserRouter(
+  //   createRoutesFromElements(
+  //     <Routes>
+  //       <Route
+  //         path="/"
+  //         element={
+  //           <div className="container my-3">
+  //             <TextForm showAlert={showAlert} mode={mode} />
+  //           </div>
+  //         }
+  //       />
+  //       <Route path="/about" element={<About mode={mode} />} />
+  //     </Routes>
+  //   )
+  // );
+
+  return (
+    <div
+      style={{
+        backgroundColor: mode === "dark" ? "grey" : "white",
+        minWidth: "100%",
+        minHeight: "100vh"
+      }}
+    >
+      <BrowserRouter>
+        <Navbar
+          mode={mode}
+          onModeToggle={(newMode) => {
+            console.log(newMode);
+            setMode(newMode);
+          }}
+          toggleMode={toggleMode}
+        />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div
+                className="container my-3"
+                style={{
+                  color: mode === "dark" ? "white" : "black",
+                  backgroundColor: mode === "dark" ? "grey" : "white",
+                }}
+              >
+                <Alert alert={alert} />
+                <TextForm mode={mode} showAlert={showAlert} />
+              </div>
+            }
+          />
+          <Route path="/about" element={<About mode={mode} />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
+
+  // return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,42 +1,80 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
-export default function TextForm(props){
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("converted to uppercase", "success");
+  };
 
-    const handleUpClick=()=>{
-    
-        let newText=text.toUpperCase();
-        setText(newText);
-    }
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
+    props.showAlert("text cleared", "success");
+  };
 
-    const handleClearClick=()=>{
-      let newText=("");
-      setText(newText);
-    }
-
-    const handleOnChange=(event)=>{
-      
-        setText(event.target.value);
-    }
-    const [text,setText] = useState("");
-    return(
-      <>
-    <div className="container">
-    <h1>{props.heading}</h1>
-  <div className="mb-3">
-   
-    <textarea  className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
-    
-  </div>
-<button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
-<button className="btn btn-primary mx-1"  onClick={handleClearClick}>Clear Text</button>
-  </div>
- <div className="container my-3">
-    <h1>Your Text Summary</h1>
-    <p>{text.split(" ").length} words and {text.length} characters </p>
-    <p>{0.008*text.split(" ").length} minutes will take to read</p>
-    <h2>Preview</h2>
-    <p>{text}</p>
- </div>
-  </>  
-    )
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("");
+  return (
+    <>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            style={{
+              backgroundColor: props.mode === "dark" ? "#13466e" : "white",
+              color: props.mode === "dark" ? "white" : "black",
+            }}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary"
+          onClick={handleUpClick}
+        >
+          Convert to UpperCase
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-1 my-1"
+          onClick={handleClearClick}
+        >
+          Clear Text
+        </button>
+      </div>
+      <div
+        className="container my-3"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <h1>Your Text Summary</h1>
+        <p>
+          {
+            text.split(" ").filter((element) => {
+              return element.length !== 0;
+            }).length
+          }{" "}
+          words and {text.length} characters{" "}
+        </p>
+        <p>
+          {0.008 *
+            text.split(" ").filter((element) => {
+              return element.length !== 0;
+            }).length}{" "}
+          minutes will take to read
+        </p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
+  );
 }
